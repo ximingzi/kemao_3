96089fd32d1c7c16b68da0bef6cb869df5f55300
root <Administrator@SC-201811241439>
2019-03-25 21:19:44 +0800

    yaunma

--- src/main/java/com/example/controller/MessageReceiverController.java
+++ src/main/java/com/example/controller/MessageReceiverController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 //controller（控制器）其实就相当于servlet，但是spring MVC把所有的serviet相关的api都屏蔽了
 @RestController
-@RequestMapping("/weixin/reciver")//访问哪个路径的时候，被此处控制器处理 textwx:表示包路径
+@RequestMapping("/kemao_3/weixin/reciver")//访问哪个路径的时候，被此处控制器处理 textwx:表示包路径
 public class MessageReceiverController {
 	//必须要有handler方法才不会出404
 	@GetMapping
